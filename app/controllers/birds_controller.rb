7137765d8dcc56d8b2a679bfd831bf910a96e3b6
Tunisia Artope <artope@yahoo.com>
2022-04-05 11:12:18 -0400

    commit tree 6d9bee1ea6799df452ab61da1dee2fe8fc72f34f

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -1,4 +1,5 @@
 class BirdsController < ApplicationController
+  wrap_parameters format: []
 
   # GET /birds
   def index
@@ -8,8 +9,15 @@ class BirdsController < ApplicationController
 
   # POST /birds
   def create
-    bird = Bird.create(name: params[:name], species: params[:species])
+
+    bird = Bird.create(params.permit(:name, :species))  #<--returns a new hash with only the params permitted.
     render json: bird, status: :created
+
+    # bird = Bird.create(params)  #<--is not allowed due to Rails built-in security; will get an error message.
+    # render json: bird, status: :created
+
+    # bird = Bird.create(name: params[:name], species: params[:species])  #<--lines 15 and 16 is valid to avoid mass assignment but can become long to name each param if there are lots of params.
+    # render json: bird, status: :created
   end
 
   # GET /birds/:id
